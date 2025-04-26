fed4b047589ee8b54bd29e742127b207af17d547
gabriel <batistutag190@gmail.com>
2025-04-26 10:17:43 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import modules as md
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Distant Reading Archive</h1>
+<p>A prototype API for distant reading of science fiction novels.</p>'''
+
+
+if __name__ == '__main__':
+	pass
